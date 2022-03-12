f9275aecabc1a2f81d2d694c249b3b06082b81cc
AmakaEguzoro <eguzoroa@gmail.com>
2022-03-12 18:31:59 +0100

    'update'

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -68,6 +68,24 @@
     <br />
     <div class="row">
       <div class="md-col-8 weather-slides">
+        <a href="https://plan-wella.netlify.app/">
+          <img src="../assets/Plan Wella.png" alt="" class="plan-wella" />
+        </a>
+
+        <br />
+        <br />
+
+        <h3 class="fets"><b>Plan Wella</b></h3>
+        <p class="fets-title">React Js</p>
+        <div>
+          <p class="about-fets">
+            A Finance planing app that helps you track your Income and Expense
+            at the click of a button. This Web Application is powered by
+            Speechly.
+          </p>
+        </div>
+      </div>
+      <!-- <div class="md-col-8 weather-slides">
         <div id="slider">
           <figure>
             <a href="https://amaka-weather-app.netlify.app/">
@@ -90,7 +108,7 @@
             desired city, and changes background based on the result.
           </p>
         </div>
-      </div>
+      </div> -->
       <div class="col-md-4 tams">
         <a href="https://amakacreditcardvalidation.netlify.app/">
           <img src="../assets/credit-card.png" alt="" class="tms" />
@@ -329,6 +347,13 @@
     left: -400%;
   }
 }
+.plan-app {
+  padding: 20px;
+}
+.plan-wella {
+  border-radius: 7px;
+  width: 90%;
+}
 .fets {
   text-align: left;
   font-family: "Montserrat", sans-serif;
@@ -351,6 +376,7 @@
   width: 776px;
 }
 .about-fets {
+  width: 80%;
   text-align: left;
   font-family: "Poppins", sans-serif;
   font-size: clamp(1em, 2vw, 1.2em);
